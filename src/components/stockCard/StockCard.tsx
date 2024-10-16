eb522c9d5c8e770bac089b1ccecf93c1c7b31a02
Katy Nakhla <katy.nakhla@femtofpa.com>
2024-10-17 01:39:10 +0300

    first commit

---
+++ src/components/stockCard/StockCard.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const StockCard = () => {
+  return (
+    <View>
+      <Text>StockCard</Text>
+    </View>
+  )
+}
+
+export default StockCard
+
+const styles = StyleSheet.create({})
