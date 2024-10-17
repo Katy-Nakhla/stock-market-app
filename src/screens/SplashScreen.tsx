95948d9ffd657c2ab80f8d6fba742881151a59ab
Katy Nakhla <katy.nakhla@femtofpa.com>
2024-10-17 16:12:23 +0300

    add develop by

--- src/screens/SplashScreen.tsx
+++ src/screens/SplashScreen.tsx
@@ -1,11 +1,11 @@
-import { StyleSheet, Animated, Image } from 'react-native'
-import React, {useState} from 'react'
-import BootSplash from "react-native-bootsplash";
+import {StyleSheet, Animated, Image, View, Text} from 'react-native';
+import React, {useState} from 'react';
+import BootSplash from 'react-native-bootsplash';
 
 type Props = {
-    onAnimationEnd: () => void;
+  onAnimationEnd: () => void;
 };
-  
+
 const SplashScreen = ({ onAnimationEnd }: Props) => {
   const [opacity] = useState(() => new Animated.Value(1));
 
@@ -24,14 +24,30 @@ const SplashScreen = ({ onAnimationEnd }: Props) => {
     },
   });
 
-  
   return (
-    <Animated.View {...container} style={[container.style, { opacity }]}>
+    <Animated.View {...container} style={[container.style, {opacity}]}>
       <Image {...logo} />
+      <Text style={styles.developBy}>Developed by</Text>
+      <Text style={styles.name}>Katrien Nakhla</Text>
     </Animated.View>
   );
-}
+};
 
-export default SplashScreen
+export default SplashScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  developBy: {
+    fontSize: 14,
+    color: '#000',
+    textAlign: 'center',
+    position: 'absolute',
+    bottom: 80,
+  },
+  name: {
+    fontSize: 18,
+    textAlign: 'center',
+    color: '#000',
+    position: 'absolute',
+    bottom: 50,
+  },
+});
